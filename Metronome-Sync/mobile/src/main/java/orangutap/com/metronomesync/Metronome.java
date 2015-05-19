953e9b8f6cf73c96c9bd4757dc956ea8f9607faf
JKereliuk <j.kereliuk@mail.utoronto.ca>
2015-05-19 11:04:59 -0400

    Hopefully my commits are linked to the correct account now

--- Metronome-Sync/mobile/src/main/java/orangutap/com/metronomesync/Metronome.java
+++ Metronome-Sync/mobile/src/main/java/orangutap/com/metronomesync/Metronome.java
@@ -14,6 +14,7 @@ import java.util.Observer;
  */
 public class Metronome implements Observer {
 
+
     private Vibrator mVibrator;         /* This controls the vibrating service */
     private boolean mIsRunning;         /* A boolean that says whether
                                         the metronome is vibrating */
